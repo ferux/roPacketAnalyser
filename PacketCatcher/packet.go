113a4ee012c37a3956eec7e7387c8eb0ee4a1f9a
Aleksandr Trushkin <total.d.darkness@gmail.com>
2018-03-28 17:39:14 -0500

    Packet Parsing has been improved

--- PacketCatcher/packet.go
+++ PacketCatcher/packet.go
@@ -34,15 +34,16 @@ func ParseChannel(conn io.Reader) chan *Packet {
 	bufreader := bufio.NewReader(conn)
 	go func() {
 		for {
-			n, err := bufreader.Read(buf)
+			_, err := bufreader.Read(buf)
 			if err != nil {
 				break
 			}
-			r := bytes.NewBuffer(buf[:n])
+			r := bytes.NewBuffer(buf)
 			for {
 				if r.Len() == 0 {
 					break
 				}
+				//TODO: Stop using string and transfer to hex
 				var packetIDint uint16
 				binary.Read(r, binary.LittleEndian, &packetIDint)
 				packetID := fmt.Sprintf("%04x", packetIDint)
@@ -62,7 +63,7 @@ func ParseChannel(conn io.Reader) chan *Packet {
 				item, ok := packetLength[packetID]
 				if ok && item > 2 {
 					takeLength = item - 2
-				} else if ok && item <= 0 {
+				} else if ok && item < 0 {
 					var ln uint16
 					binary.Read(r, binary.LittleEndian, &ln)
 					if ln == 0 {
@@ -76,6 +77,10 @@ func ParseChannel(conn io.Reader) chan *Packet {
 				if takeLength == 0 {
 					continue
 				}
+				if takeLength < 0 {
+					//fmt.Printf("Length of Packet is less than 0. Breaking: %v", takeLength)
+					break
+				}
 				body := r.Next(takeLength)
 				packetc <- New(packetID, body, isPacketSent)
 			}
@@ -84,14 +89,102 @@ func ParseChannel(conn io.Reader) chan *Packet {
 	return packetc
 }
 
+//ParseChannelV2 should be an updated version with refactored logic
 func ParseChannelV2(conn io.Reader) chan *Packet {
-	var isPacketSent bool
+	var isSent bool
 	packetc := make(chan *Packet, 0)
-	// buf := make([]byte, 65536)
-	// bufreader := bufio.NewReader(conn)
 	go func() {
-		p := New("meh", []byte("y"), isPacketSent)
-		packetc <- p
+		r := bufio.NewReader(conn)
+		rBuffer1 := make([]byte, 2048)
+		errCount := 0
+		for {
+			n, err := r.Read(rBuffer1)
+			if err != nil {
+				errCount++
+				if errCount > 5 {
+					//fmt.Println("Have more than 5 errors while reading from connection. Exiting.")
+					close(packetc)
+					break
+				}
+				//fmt.Printf("Got an error while reading from connection: %v\n", err)
+				continue
+			}
+			rBuffer := rBuffer1[:n]
+			next := true
+			//fmt.Printf("Incoming packet is:[%d] %x\n", n, rBuffer)
+			if n < 2 {
+				//fmt.Println("Array length is less then 2. Wrong packet. Discarding")
+				continue
+			}
+			dataReader := bytes.NewBuffer(rBuffer)
+			for dataReader.Len() > 0 && next {
+				var pid uint16
+				if err := binary.Read(dataReader, binary.LittleEndian, &pid); err != nil {
+					errCount++
+					//fmt.Printf("Got an error while decoding dataReader: %v\n", err)
+					next = false
+					continue
+				}
+				if pid == 0 {
+					//fmt.Println("Found strange packet which ID is 0x0000. Skiping")
+					continue
+				}
+				switch pid {
+				case 0x5252:
+					isSent = false
+					continue
+				case 0x5353:
+					isSent = true
+					continue
+				case 0x592c:
+					continue
+				case 0x007f:
+					continue
+				case 0x09a1:
+					continue
+				default:
+				}
+				pidString := fmt.Sprintf("%04x", pid)
+				//fmt.Printf("Found packetID: 0x%s\n", pidString)
+				if dataReader.Len() == 0 {
+					//fmt.Println("Packet doesn't contain any data.")
+					packetc <- New(pidString, make([]byte, 0), isSent)
+					continue
+				}
+				var plen int16
+				if l, ok := packetLength[pidString]; !ok || l == -1 {
+					//fmt.Printf("Present in map: %v\tLength: %d\nVariable Length. Extracting...", ok, l)
+					if err := binary.Read(dataReader, binary.LittleEndian, &plen); err != nil {
+						errCount++
+						//fmt.Printf("Got an error while decoding dataReader: %v\n", err)
+						next = false
+						continue
+					}
+					//fmt.Printf("Got packet length: %d\n", plen)
+					plen -= 2
+				} else if ok {
+					//fmt.Printf("Present in map: [%v] Length: [%d] Good!\n", ok, l)
+					plen = int16(l)
+				} else if l == 0 {
+					//fmt.Println("Found zero-body packet")
+					packetc <- New(pidString, make([]byte, 0), isSent)
+					continue
+				}
+				plen -= 2
+				//fmt.Printf("Rest packetlength is: [%d]. dataReader length is: [%d]\n", plen, dataReader.Len())
+				if plen < 0 {
+					packetc <- New(pidString, make([]byte, 0), isSent)
+					continue
+				}
+				if plen > 0 && plen <= int16(dataReader.Len()) {
+					packetData := dataReader.Next(int(plen)) //TODO: Look carefuly at this cauz maybe reader can't perform zero-len reading
+					//fmt.Printf("Prepared packetBody: %x\n", packetData)
+					packetc <- New(pidString, packetData, isSent)
+
+				}
+			}
+
+		}
 	}()
 	return packetc
 }
--- main.go
+++ main.go
@@ -19,6 +19,8 @@ var wg sync.WaitGroup
 //TODO: Remove vending from this package
 func main() {
 	startListen()
+
+	// startDial()
 }
 
 func startDial() {
@@ -64,22 +66,25 @@ func startListen() {
 		}
 		log.Println("Connection from client accepted")
 		serveConnV3(conn)
+		fmt.Println("Connection has finished. Listening for a new one")
 	}
 }
 
 func serveConnV3(conn net.Conn) {
-	pc := PacketCatcher.ParseChannel(conn)
+	pc := PacketCatcher.ParseChannelV2(conn)
+	// pc := PacketCatcher.ParseChannel(conn)
 	for packet := range pc {
 		go servePacket(packet)
 		// servePacket(packet)
 	}
+	fmt.Println("ServeConnV3 ended")
 }
 
 func servePacket(p *PacketCatcher.Packet) {
 	m := PacketTypes.MakeMap(p)
 	if m == nil {
-		// log.Printf("%s\n", p.String())
-		// log.Printf("%x\n", p.Body)
+		log.Printf("%s\n", p.String())
+		log.Printf("%x\n", p.Body)
 	} else {
 		switch p.PacketID {
 		case "0064":
@@ -87,84 +92,84 @@ func servePacket(p *PacketCatcher.Packet) {
 			output := fmt.Sprintf("Logged in as %s\n", login)
 			log.Println(output)
 		case "02e1":
-			// p := PacketTypes.Packet02e1{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// log.Printf("[%d] did %d damage to %d", p.GID, p.Damage, p.TargetGID)
+			p := PacketTypes.Packet02e1{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			log.Printf("[%d] did %d damage to %d", p.GID, p.Damage, p.TargetGID)
 		case "0230":
-			// p := PacketTypes.Packet0230{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// switch p.State {
-			// case 1:
-			// 	etaMin := time.Duration((911-p.Data)*10) * time.Minute
-			// 	etaDate := time.Now().Add(etaMin)
-			// 	rate := float32(p.Data) / 911.0 * 100.0
-			// 	log.Printf("Homunculus intimacy level changed to %d (Left: %d [%5.2f%%]) (ETA: %.0f / %v)", p.Data, 911-p.Data, rate, etaMin.Minutes(), etaDate.Format("02-01-2006 15:04:05"))
-			// case 2:
-			// 	log.Printf("Homunculus hungry level changed to %d\n", p.Data)
-			// 	if p.Data < 20 {
-			// 		beeep.Notify("Homunculus", fmt.Sprintf("Hungry level is %d", p.Data), "ro.png")
-			// 	}
-			// }
+			p := PacketTypes.Packet0230{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			switch p.State {
+			case 1:
+				etaMin := time.Duration((911-p.Data)*10) * time.Minute
+				etaDate := time.Now().Add(etaMin)
+				rate := float32(p.Data) / 911.0 * 100.0
+				log.Printf("Homunculus intimacy level changed to %d (Left: %d [%5.2f%%]) (ETA: %.0f / %v)", p.Data, 911-p.Data, rate, etaMin.Minutes(), etaDate.Format("02-01-2006 15:04:05"))
+			case 2:
+				log.Printf("Homunculus hungry level changed to %d\n", p.Data)
+				if p.Data < 20 {
+					beeep.Notify("Homunculus", fmt.Sprintf("Hungry level is %d", p.Data), "ro.png")
+				}
+			}
 		case "08c8":
-			// p := PacketTypes.Packet08c8{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// log.Printf("Character [%d] did %d damage to %d\n", p.GID, p.Damage, p.TargetGID)
+			p := PacketTypes.Packet08c8{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			log.Printf("Character [%d] did %d damage to %d\n", p.GID, p.Damage, p.TargetGID)
 		case "0080":
-			// p := PacketTypes.Packet0080{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// log.Printf("Item [%d] of type [%d] vanished", p.GID, p.Type)
+			p := PacketTypes.Packet0080{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			log.Printf("Item [%d] of type [%d] vanished", p.GID, p.Type)
 		case "07f6":
-			// p := PacketTypes.Packet07F6{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// expType := ""
-			// switch p.VarID {
-			// case 1:
-			// 	expType = "Base"
-			// 	totalExp.addBaseExp(p.Amount)
-			// 	log.Printf("[Kills: %5d] Gained [%5d] %s exp. [TotalBaseExp: %7d] [Per Hour: %9.2f]",
-			// 		totalExp.getKills(), p.Amount, expType, totalExp.getBaseExp(), totalExp.getBasePerHour())
-			// case 2:
-			// 	expType = "Job "
-			// 	totalExp.addJobExp(p.Amount)
-			// 	log.Printf("[Kills: %5d] Gained [%5d] %s exp. [TotalJobExp:  %7d] [Per Hour: %9.2f]",
-			// 		totalExp.getKills(), p.Amount, expType, totalExp.getJobExp(), totalExp.getJobPerHour())
-			// default:
-			// 	expType = string(p.ExpType)
-			// }
-			// log.Printf("[KillAmount: %d] Gained [%5d] %s exp. [TotalExp: %7d|%7d] [Per Hour: %9.2f|%9.2f]", totalExp.getKills(), p.Amount, expType, totalExp.getBaseExp(), totalExp.getJobExp(), totalExp.getBasePerHour(), totalExp.getJobPerHour())
+			p := PacketTypes.Packet07F6{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			expType := ""
+			switch p.VarID {
+			case 1:
+				expType = "Base"
+				totalExp.addBaseExp(p.Amount)
+				log.Printf("[Kills: %5d] Gained [%5d] %s exp. [TotalBaseExp: %7d] [Per Hour: %9.2f]",
+					totalExp.getKills(), p.Amount, expType, totalExp.getBaseExp(), totalExp.getBasePerHour())
+			case 2:
+				expType = "Job "
+				totalExp.addJobExp(p.Amount)
+				log.Printf("[Kills: %5d] Gained [%5d] %s exp. [TotalJobExp:  %7d] [Per Hour: %9.2f]",
+					totalExp.getKills(), p.Amount, expType, totalExp.getJobExp(), totalExp.getJobPerHour())
+			default:
+				expType = string(p.ExpType)
+			}
+			log.Printf("[KillAmount: %d] Gained [%5d] %s exp. [TotalExp: %7d|%7d] [Per Hour: %9.2f|%9.2f]", totalExp.getKills(), p.Amount, expType, totalExp.getBaseExp(), totalExp.getJobExp(), totalExp.getBasePerHour(), totalExp.getJobPerHour())
 		case "022e":
-			// p := PacketTypes.Packet022E{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// etaMin := time.Duration((911-p.NRelationship)*10) * time.Minute
-			// etaDate := time.Now().Add(etaMin)
-			// rate := float32(p.NRelationship) / 911.0 * 100.0
-			// log.Printf("Homunculus [%s]: Intimacy: %d (%5.2f%%)\tHunger: %d\tETA: %.0fm (~%.2fh)\tDate: %v", p.SzName, p.NRelationship, rate, p.NFullness, etaMin.Minutes(), etaMin.Hours(), etaDate)
+			p := PacketTypes.Packet022E{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			etaMin := time.Duration((911-p.NRelationship)*10) * time.Minute
+			etaDate := time.Now().Add(etaMin)
+			rate := float32(p.NRelationship) / 911.0 * 100.0
+			log.Printf("Homunculus [%s]: Intimacy: %d (%5.2f%%)\tHunger: %d\tETA: %.0fm (~%.2fh)\tDate: %v", p.SzName, p.NRelationship, rate, p.NFullness, etaMin.Minutes(), etaMin.Hours(), etaDate)
 		case "00b0":
-			// p := PacketTypes.Packet00B0{}
-			// if err := p.Populate(m); err != nil {
-			// 	log.Printf("Error: %v\n", err)
-			// 	return
-			// }
-			// txt := PacketCatcher.GetVarID(p.VarID)
-			// log.Printf("Parameter %s changed to %d", txt, p.Count)
+			p := PacketTypes.Packet00B0{}
+			if err := p.Populate(m); err != nil {
+				log.Printf("Error: %v\n", err)
+				return
+			}
+			txt := PacketCatcher.GetVarID(p.VarID)
+			log.Printf("Parameter %s changed to %d", txt, p.Count)
 		case "09dd":
 			pd := PacketTypes.Packet09DD{}
 			if err := pd.Populate(m); err != nil {
@@ -177,7 +182,7 @@ func servePacket(p *PacketCatcher.Packet) {
 				beeep.Notify("TOMB", notify, "ro.png")
 			}
 		default:
-			// log.Printf("Packet: %v\n", m)
+			log.Printf("Packet: %v\n", m)
 		}
 	}
 }
